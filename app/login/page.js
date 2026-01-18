a9b215fba130d0c0c5fb1f1ce7c8daefba94a536
workharshit6-cmyk <workharshit6@gmail.com>
2026-01-18 15:11:24 +0530

    add fresh login route

---
+++ app/login/page.js
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Login Page</h1>
+
+      <input placeholder="Email" />
+      <br /><br />
+
+      <input type="password" placeholder="Password" />
+      <br /><br />
+
+      <button>Login</button>
+    </div>
+  );
+}
